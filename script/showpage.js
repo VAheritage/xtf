19551f0fbfa6c09d064a76272fc1bd52e937d515
sdm7g <sdm7g@895da4cf-db3f-4dd9-8961-e12c2c55792c>
2019-07-18 22:41:42 +0000

    correct iiif page image size
    git-svn-id: https://subversion.lib.virginia.edu/repos/blacklight/xml_styling/xtf/trunk@3939 895da4cf-db3f-4dd9-8961-e12c2c55792c

--- script/showpage.js
+++ script/showpage.js
@@ -5,7 +5,7 @@ $(function () {
 			var alt = $(this) .attr('alt');
             var repo = $('#' + pid + '_container').attr('class').split(" ")[1];
             if (repo != undefined ) {
-							image_url = "https://iiif.lib.virginia.edu/iiif/uva-lib:" + pid + "/full/1600,1600/0/default.jpg";
+							image_url = "https://iiif.lib.virginia.edu/iiif/uva-lib:" + pid + "/full/,1600/0/default.jpg";
               // image_url = "http://" + repo + ".lib.virginia.edu:8080/fedora/objects/uva-lib:" + pid + "/methods/djatoka%3AStaticSDef/getScaled?maxWidth=1600&maxHeight=1600";
             } else {
               image_url = "http://" + "fedora-prod01" + ".lib.virginia.edu:8080/fedora/get/uva-lib:" + pid + "/uva-lib-bdef:102/getScreen";
