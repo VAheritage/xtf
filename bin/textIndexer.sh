c91ba7efa157d29966879e8b919984d05a2e47c4
Robert Haschart <rh9ec@virginia.edu>
2023-08-15 02:48:55 +0000

    adding shell script to run indexer.

---
+++ bin/textIndexer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BASEBINDIR=$( (cd -P $(dirname $0) && pwd) )
+
+HOMEDIR=$(dirname $BASEBINDIR)
+
+CLASSPATH="$HOMEDIR/WEB-INF/classes:$HOMEDIR/WEB-INF/lib/*"
+
+JAVAARGS="-Xms50m -Xmx1000m -Dxtf.home=$HOMEDIR -DentityExpansionLimit=128000 -enableassertions"
+
+MAINCLASS="org.cdlib.xtf.textIndexer.TextIndexer"
+
+java -classpath $CLASSPATH $JAVAARGS $MAINCLASS $*
