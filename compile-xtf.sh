2ada233cee0b359eb1d7f50c1b6d7a7eeecf8951
Michael Durbin <md5wz@virginia.edu>
2025-02-14 11:14:12 -0500

    Revert "Moved docker-based rebuild scripts to this project (from xtf-docker)."
    This reverts commit 82157f20e900ab0ba56a8f7c10780e42576da2d5.

--- compile-xtf.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-PWD=`pwd`
-docker build -f rebuildxtf.Dockerfile -t xtf-builder .
-docker run -it --ulimit nofile=16384:16384 -v $PWD:/temp/xtf xtf-builder 
-cp $PWD/WEB-INF/dist/xtf.jar $PWD/WEB-INF/lib/
